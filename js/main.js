cd105d5cde73a0b579c0401b5004590cbfb683b4
Lucia Machado <lucia.machado.olascoaga@gmail.com>
2023-11-01 20:37:22 -0300

    Arreglos de animacion

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-/*Swal.fire({
-    title: '¡Bienvenido a mi mundo digital!',
-    showClass: {
-      popup: 'animate__animated animate__fadeInDown'
-    },
-    hideClass: {
-      popup: 'animate__animated animate__fadeOutUp'
-    },
-    confirmButtonText: '¡Gracias!'
-  })*/
+
+
+  
+  
+  
